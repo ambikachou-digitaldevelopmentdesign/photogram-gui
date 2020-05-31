0507977a6004ef062ca5aa3d88f17568e09f28d6
ambikarchou <ambika.chou@kellogg.northwestern.edu>
2020-05-31 20:45:44 +0000

    Add user on users page works better

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,21 @@ class UsersController < ApplicationController
   end
 
   def add_row
-    the_username = params.fetch("input_username")
-    u = User.new
-    u.username = the_username
-    u.save
-    render({ :template => "user_templates/add_row.html.erb" })
+    new_user_username = params.fetch("input_username")
+    new_user = User.new
+    new_user.username = new_user_username
+    new_user.save
+    @user = new_user
+    render({ :template => "user_templates/show_details.html.erb" })
+  end
+
+  def update_row
+    # old_username = params.fetch("old_username")
+    # new_username = params.fetch("update_username")
+    # user = User.where({ :username => old_username})
+    # user.username = new_username
+    # user.save
+    render({ :template => "user_templates/list_of_users.html.erb" })
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get("/users/:a_username", { :controller => "users", :action => "details" })
   get("/insert_user_record", { :controller => "users", :action => "add_row"})
+  get("/update_username", { :controller => "users", :action => "update_row"})
 
   get("/photos", { :controller => "photos", :action => "index" })
   get("/users", { :controller => "users", :action => "index" })
